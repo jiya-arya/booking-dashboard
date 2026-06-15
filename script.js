fd5c43cf9d22b77520bbc0b1e737e0257f91ac1f
Jiya <jiyaarya1205@gmail.com>
2026-06-15 21:35:06 +0530

    feat: implement real-time booking search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("script.js loaded");
 
-// DOM Elements
+// dom elements
 const bookingForm = document.getElementById("booking-form");
 
 const nameInput = document.getElementById("name");
@@ -11,76 +11,83 @@ const timeInput = document.getElementById("time");
 
 const bookingList = document.getElementById("booking-list");
 
-const submitButton = bookingForm.querySelector("button");
+const searchInput = document.getElementById("search");
 
-const totalBookingsElement = document.getElementById("total-bookings");
+const submitButton =
+  bookingForm.querySelector("button");
 
-const upcomingBookingsElement = document.getElementById("upcoming-bookings");
+const totalBookingsElement =
+  document.getElementById("total-bookings");
 
-const completedBookingsElement = document.getElementById("completed-bookings");
+const upcomingBookingsElement =
+  document.getElementById("upcoming-bookings");
 
-const cancelledBookingsElement = document.getElementById("cancelled-bookings");
+const completedBookingsElement =
+  document.getElementById("completed-bookings");
 
-// Application State
+const cancelledBookingsElement =
+  document.getElementById("cancelled-bookings");
+
+//array to store bookings
 let bookings = [];
 
-// Edit Booking State
 let editBookingId = null;
 
-// Form Submit Event
-bookingForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+//form submission
+bookingForm.addEventListener(
+  "submit",
+  function (event) {
+    event.preventDefault();
 
-  // Create Booking Object
-  const booking = {
-    id: Date.now(),
-    name: nameInput.value,
-    phone: phoneInput.value,
-    service: serviceInput.value,
-    date: dateInput.value,
-    time: timeInput.value,
-    status: "upcoming",
-  };
+    const booking = {
+      id: Date.now(),
+      name: nameInput.value,
+      phone: phoneInput.value,
+      service: serviceInput.value,
+      date: dateInput.value,
+      time: timeInput.value,
+      status: "upcoming",
+    };
 
-  // Add booking to state
-  // bookings.push(booking);
+    // Add New Booking
+    if (editBookingId === null) {
+      bookings.push(booking);
+    }
 
-  // If editBookingId is null, we are adding a new booking. Otherwise, we are updating an existing booking.
-  if (editBookingId === null) {
-    bookings.push(booking);
-  } else {
-    bookings = bookings.map((item) => {
-      if (item.id === editBookingId) {
-        return {
-          // ... in short is a way to copy all properties of an object into a new object.
-          ...booking,
-          id: editBookingId,
-        };
-      }
+    // Update Existing Booking
+    else {
+      bookings = bookings.map((item) => {
+        if (item.id === editBookingId) {
+          return {
+            ...booking,
+            id: editBookingId,
+          };
+        }
 
-      return item;
-    });
+        return item;
+      });
 
-    editBookingId = null;
+      editBookingId = null;
 
-    submitButton.textContent =
-      "Add Booking";
+      submitButton.textContent =
+        "Add Booking";
+    }
+
+    bookingForm.reset();
+
+    renderBookings();
+    updateStatistics();
   }
+);
 
-  // Reset form
-  bookingForm.reset();
-
-  // Update UI
-  renderBookings();
-  updateStatistics();
-});
-
-// Render Bookings
-function renderBookings() {
+//render bookings
+function renderBookings(
+  bookingsToRender = bookings
+) {
   bookingList.innerHTML = "";
 
-  // Guard Clause
-  if (bookings.length === 0) {
+  // Empty State
+  if (bookingsToRender.length === 0) {
     bookingList.innerHTML = `
       <p class="empty-state">
         No bookings yet. Add your first booking above.
@@ -90,18 +97,61 @@ function renderBookings() {
     return;
   }
 
-  // Render each booking card
-  bookings.forEach((booking) => {
+  // Render Cards
+  bookingsToRender.forEach((booking) => {
     const bookingCard = `
       <div class="booking-card">
+
         <h3>${booking.name}</h3>
+
         <p>${booking.phone}</p>
+
         <p>${booking.service}</p>
+
         <p>${booking.date}</p>
+
         <p>${booking.time}</p>
-        <p>${booking.status}</p>
+
+        <select
+          class="status-select"
+          data-id="${booking.id}"
+        >
+          <option
+            value="upcoming"
+            ${
+              booking.status === "upcoming"
+                ? "selected"
+                : ""
+            }
+          >
+            Upcoming
+          </option>
+
+          <option
+            value="completed"
+            ${
+              booking.status === "completed"
+                ? "selected"
+                : ""
+            }
+          >
+            Completed
+          </option>
+
+          <option
+            value="cancelled"
+            ${
+              booking.status === "cancelled"
+                ? "selected"
+                : ""
+            }
+          >
+            Cancelled
+          </option>
+        </select>
 
         <div class="card-actions">
+
           <button
             class="edit-btn"
             data-id="${booking.id}"
@@ -115,87 +165,178 @@ function renderBookings() {
           >
             Delete
           </button>
+
         </div>
+
       </div>
     `;
 
     bookingList.innerHTML += bookingCard;
   });
 
-  // Attach delete button events
+  // Delete Buttons
   const deleteButtons =
     document.querySelectorAll(".delete-btn");
 
-  //in short dataset is a way to access data attributes in HTML.
   deleteButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const bookingId = Number(button.dataset.id);
+    button.addEventListener(
+      "click",
+      function () {
+        const bookingId = Number(
+          button.dataset.id
+        );
 
-      deleteBooking(bookingId);
-    });
+        deleteBooking(bookingId);
+      }
+    );
   });
 
-  // Attach edit button events
+  // Edit Buttons
   const editButtons =
     document.querySelectorAll(".edit-btn");
 
   editButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const bookingId = Number(button.dataset.id);
+    button.addEventListener(
+      "click",
+      function () {
+        const bookingId = Number(
+          button.dataset.id
+        );
 
-      editBooking(bookingId);
-    });
+        editBooking(bookingId);
+      }
+    );
+  });
+
+  // Status Dropdowns
+  const statusSelects =
+    document.querySelectorAll(
+      ".status-select"
+    );
+
+  statusSelects.forEach((select) => {
+    select.addEventListener(
+      "change",
+      function () {
+        const bookingId = Number(
+          select.dataset.id
+        );
+
+        const newStatus =
+          select.value;
+
+        updateBookingStatus(
+          bookingId,
+          newStatus
+        );
+      }
+    );
   });
 }
 
-// Delete Booking
+//delete booking status
 function deleteBooking(id) {
   bookings = bookings.filter(
     (booking) => booking.id !== id
   );
 
   renderBookings();
+  updateStatistics();
 }
 
-// Edit Booking
+// edit booking status
 function editBooking(id) {
   const booking = bookings.find(
     (booking) => booking.id === id
   );
 
-  // Populate form with booking data
   nameInput.value = booking.name;
   phoneInput.value = booking.phone;
-  serviceInput.value = booking.service;
+  serviceInput.value =
+    booking.service;
   dateInput.value = booking.date;
   timeInput.value = booking.time;
 
-  // Set edit state
   editBookingId = id;
 
-  // Change button text of form to indicate update mode
-  submitButton.textContent = "Update Booking";
-
+  submitButton.textContent =
+    "Update Booking";
 }
 
-// Update Statistics
+//update booking status
+function updateBookingStatus(
+  id,
+  newStatus
+) {
+  bookings = bookings.map((booking) => {
+    if (booking.id === id) {
+      return {
+        ...booking,
+        status: newStatus,
+      };
+    }
+
+    return booking;
+  });
+
+  renderBookings();
+  updateStatistics();
+}
+
+//statistics
 function updateStatistics() {
-  const totalBookings = bookings.length;
+  const totalBookings =
+    bookings.length;
 
-  const upcomingBookings = bookings.filter(
-    (booking) => booking.status === "upcoming"
-  ).length;
+  const upcomingBookings =
+    bookings.filter(
+      (booking) =>
+        booking.status === "upcoming"
+    ).length;
 
-  const completedBookings = bookings.filter(
-    (booking) => booking.status === "completed"
-  ).length;
+  const completedBookings =
+    bookings.filter(
+      (booking) =>
+        booking.status === "completed"
+    ).length;
 
-  const cancelledBookings = bookings.filter(
-    (booking) => booking.status === "cancelled"
-  ).length;
+  const cancelledBookings =
+    bookings.filter(
+      (booking) =>
+        booking.status === "cancelled"
+    ).length;
 
-  totalBookingsElement.textContent = totalBookings;
-  upcomingBookingsElement.textContent = upcomingBookings;
-  completedBookingsElement.textContent = completedBookings;
-  cancelledBookingsElement.textContent = cancelledBookings;
+  totalBookingsElement.textContent =
+    totalBookings;
+
+  upcomingBookingsElement.textContent =
+    upcomingBookings;
+
+  completedBookingsElement.textContent =
+    completedBookings;
+
+  cancelledBookingsElement.textContent =
+    cancelledBookings;
 }
+
+//search functionality
+searchInput.addEventListener(
+  "input",
+  function () {
+    const searchTerm =
+      searchInput.value.toLowerCase();
+
+    const filteredBookings =
+      bookings.filter((booking) =>
+        booking.name
+          .toLowerCase()
+          .includes(searchTerm)
+      );
+
+    renderBookings(filteredBookings);
+  }
+);
+
+// Initial Render - meaning when the page loads, we want to render the bookings and update the statistics
+renderBookings();
+updateStatistics();
